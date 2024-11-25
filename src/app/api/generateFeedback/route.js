0d7678d8337eb57de0d2ab2628ca3bded0f7199a
opuopu <nurmdopu428@gmail.com>
2024-11-25 11:20:07 +0600

    feat: added open ai

--- src/app/api/generateFeedback/route.js
+++ src/app/api/generateFeedback/route.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const client = new OpenAI({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req) {
